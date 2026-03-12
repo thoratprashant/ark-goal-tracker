8af317029a5df0bb98ecd0bf42cc46b5f5e542df
thoratprashant <prashant.thorat@arkenea.com>
2026-03-12 19:55:06 +0530

    Completed Admin Dashboard

--- src/app/features/admin/dashboard/dashboard.ts
+++ src/app/features/admin/dashboard/dashboard.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule,RouterLink,MatIconModule ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
@@ -27,4 +30,63 @@ cards = [
     icon: 'images/upload.svg'
   }
 ];
+
+qacards = [
+  {
+    title: 'User Management',
+    description: 'Add, edit, or remove users',
+    icon: 'images/poeple-blue.svg',
+    link: '/admin/us'
+  }, 
+  {
+    title: 'Permissions',
+    description: 'Configure role-based access',
+    icon: 'images/permission-blue.svg',
+    link: '/admin/us'
+  }, 
+    {
+    title: 'Data Import',
+    description: 'Upload student and assessment data',
+    icon: 'images/export-blue.svg',
+    link: '/admin/us'
+  }, 
+    {
+    title: 'Student Insights',
+    description: 'View comprehensive student data',
+    icon: 'images/poeple-blue.svg',
+    link: '/admin/us'
+  }, 
+]
+
+activities = [
+  {
+    title: 'User Import',
+    user: 'System',
+    time: '10 minutes ago',
+    icon: 'check_circle',
+    type: 'success'
+  },
+  {
+    title: 'Data Import Failed',
+    user: 'District Admin',
+    time: '2 hours ago',
+    icon: 'warning',
+    type: 'warning'
+  },
+  {
+    title: 'Goal Updated',
+    user: 'Admin',
+    time: '20 minutes ago',
+    icon: 'check_circle',
+    type: 'success'
+  },
+  {
+    title: 'New Principal Added',
+    user: 'Super Admin',
+    time: '3 hours ago',
+    icon: 'check_circle',
+    type: 'success'
+  },
+];
+
 }
