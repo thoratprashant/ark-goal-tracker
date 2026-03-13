ed907cbabf59c891605ac9727cd925bb4a55866b
thoratprashant <prashant.thorat@arkenea.com>
2026-03-13 11:06:49 +0530

    UI

--- src/app/features/admin/recent-activity/recent-activity.ts
+++ src/app/features/admin/recent-activity/recent-activity.ts
@@ -4,14 +4,65 @@ import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import {MatSelectModule} from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
+import { CommonService } from '../../../core/helper/common.service'; 
 @Component({
   selector: 'app-recent-activity',
-  imports: [CommonModule,RouterLink,MatIconModule,MatSelectModule,MatFormFieldModule],
+   standalone: true,
+  imports: [CommonModule,RouterLink,MatIconModule,MatSelectModule,MatFormFieldModule,],
   templateUrl: './recent-activity.html',
   styleUrl: './recent-activity.scss',
 })
-export class RecentActivity {
+export class RecentActivity { 
+
+  constructor( private commonService: CommonService ) {}
+
+  loadMore(){ 
+    this.commonService.showLoader();
+
+       setTimeout(() => {
+
+        const moreActivities = [
+          {
+            title: '001',
+            user: 'System',
+            time: '10 minutes ago',
+            icon: 'check_circle',
+            type: 'success'
+          },
+          {
+            title: '002',
+            user: 'District Admin',
+            time: '2 hours ago',
+            icon: 'warning',
+            type: 'warning'
+          },
+          {
+            title: '003',
+            user: 'Admin',
+            time: '20 minutes ago',
+            icon: 'check_circle',
+            type: 'success'
+          }
+        ]; 
+
+         // append new data
+      this.activities = [...this.activities, ...moreActivities];
+
+    // hide loader
+    this.commonService.hideLoader();
+
+    // scroll to bottom of page
+    setTimeout(() => {
+      window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+      });
+    }, 100);
+
+  }, 1500);  
+} 
+  
+  
   activities = [
     {
       title: 'User Import',
