6387184b71ceb77f0b7aea6d1cff117a1eebcb29
thoratprashant <prashant.thorat@arkenea.com>
2026-03-25 14:47:33 +0530

    Working on Admin login

---
+++ src/app/features/auth/admin-login/admin-login.component.ts
@@ -0,0 +1,51 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, inject, NgZone } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Router, RouterLink } from '@angular/router';  
+import { CommonService } from '../../../core/helper/common.service';
+@Component({
+  selector: 'app-admin-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, MatButtonModule, MatIconModule],
+  templateUrl: './admin-login.component.html',
+  styleUrls: ['./admin-login.component.scss'],
+})
+export class AdminLoginComponent {
+ 
+  loginForm!: FormGroup;
+  showNewPassword = false;
+
+  constructor(private fb: FormBuilder,private commonService: CommonService,private router: Router,) {}
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  toggleNewPassword() {
+    this.showNewPassword = !this.showNewPassword;
+  }
+
+  submitForm() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    } 
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  login(){
+   this.commonService.showLoader();
+     setTimeout(() => {
+    this.commonService.hideLoader();
+    this.router.navigate(['/admin/dashboard']);
+  }, 2000);
+  }
+}
--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -5,11 +5,12 @@ export const AUTH_ROUTES: Routes = [
     path: 'login',
     loadComponent: () => import('./login/login.component').then(c => c.LoginComponent),
     data: { title: 'Login' }
-  },
+  }, 
   {
-    path: 'sign-up',
-    loadComponent: () => import('./signup/signup.component').then(c => c.SignupComponent),
-  },
+    path: 'admin-login',
+    loadComponent: () => import('./admin-login/admin-login.component').then(c => c.AdminLoginComponent),
+    data: { title: 'Admin Login' }
+  }, 
   {
     path: 'forgot-password',
     loadComponent: () => import('./forgot-password/forgot-password').then(c => c.ForgotPasswordComponent),
@@ -17,9 +18,5 @@ export const AUTH_ROUTES: Routes = [
   {
     path: 'reset-password',
     loadComponent: () => import('./reset-password/reset-password').then(c => c.ResetPassword),
-  },
-  {
-    path: 'set-password',
-    loadComponent: () => import('./set-password/set-password').then(c => c.SetPassword),
-  },
+  }
 ];
