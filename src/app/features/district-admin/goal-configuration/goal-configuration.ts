eaa9e1f0ee4261c4430db95641501a7961c806bd
thoratprashant <prashant.thorat@arkenea.com>
2026-03-31 10:15:04 +0530

    district-admin/goal-configuration

--- src/app/features/district-admin/goal-configuration/goal-configuration.ts
+++ src/app/features/district-admin/goal-configuration/goal-configuration.ts
@@ -1,11 +1,57 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
+
+type SortableFields = 'name' | 'goal' | 'predicted' | 'status' | 'yoy';
+type SortDirection = 'asc' | 'desc';
+
+interface SubjectRow {
+  name: string;
+  goal: number;
+  predicted: number;
+  yoy: number;
+  type: 'safe' | 'double' | 'stretch';
+}
+
+interface SchoolRow {
+  name: string;
+  goal: number;
+  predicted: number;
+  yoy: number;
+  expanded: boolean;
+  children: SubjectRow[];
+}
+
+interface TableRow {
+  name: string;
+  goal: number;
+  predicted: number;
+  status: string;
+  yoy: number;
+  expanded: boolean;
+  children: SchoolRow[];
+}
 
 @Component({
   selector: 'app-goal-configuration',
-  imports: [],
+  imports: [MatTooltipModule,MatIconModule, CommonModule, MatButtonModule, MatSelectModule, MatFormFieldModule],
   templateUrl: './goal-configuration.html',
   styleUrl: './goal-configuration.scss',
 })
 export class GoalConfiguration {
+  progressList = [
+    { label: 'Progress', value: 100, color: '#3B82F6' }
+  ]
 
+  
 }
+
+
+
+ 
+
