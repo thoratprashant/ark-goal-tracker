f45b84735bab07798a4275a6f15adfcd83a005f0
thoratprashant <prashant.thorat@arkenea.com>
2026-04-07 11:16:56 +0530

    Auto-generated commit

---
+++ chunk-ETL3GCGF.js
@@ -0,0 +1,1 @@
+import"./chunk-7CGTOI24.js";var t=[{path:"goal-configuration",loadComponent:()=>import("./chunk-OIRIOZ2Y.js").then(o=>o.GoalConfiguration),data:{title:"Goal Configuration"}}];export{t as DISTRICTADMINLAYOUT_ROUTES};
