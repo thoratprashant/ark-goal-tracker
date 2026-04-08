7304fc1dd50a70da97e166aae3e4b2ccd3d142a9
satyajitarkenea24 <satyajit@arkenea.com>
2026-04-08 11:00:17 +0530

    Auto-generated commit

--- chunk-5WIPSEWM.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-7CGTOI24.js";var t=[{path:"goal-evaluation",loadComponent:()=>import("./chunk-B6QWNUT5.js").then(o=>o.GoalEvaluation),data:{title:"goal evaluation"}},{path:"diagnostic-breakdown",loadComponent:()=>import("./chunk-EVN4TIL7.js").then(o=>o.DiagnosticBreakdown),data:{title:"diagnostic breakdown"}}];export{t as PRINCIPALLAYOUT_ROUTES};
