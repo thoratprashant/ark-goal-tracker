58ed6d0eddaad8e8bcf68cff97bb9771e323485a
satyajitarkenea24 <satyajit@arkenea.com>
2026-05-11 17:11:01 +0530

    ui issues resolved

--- src/app/features/principal/subject-drill-down/subject-drill-down.ts
+++ src/app/features/principal/subject-drill-down/subject-drill-down.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectorRef, Component, ElementRef, HostListener, signal, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { PerformanceSummary } from '../../comman/performance-summary/performance-summary';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
@@ -25,7 +24,7 @@ interface TableRow {
 
 @Component({
   selector: 'app-subject-drill-down',
-  imports: [MatIconModule, CommonModule, MatButtonModule, MatSelectModule, MatFormFieldModule, MatButtonToggleModule, PerformanceSummary],
+  imports: [MatIconModule, CommonModule, MatButtonModule, MatSelectModule, MatFormFieldModule, MatButtonToggleModule],
   templateUrl: './subject-drill-down.html',
   styleUrl: './subject-drill-down.scss',
 })
