9f8dd257e07cbf390a7a4e858d358a1d1bd099d4
satyajitarkenea24 <satyajit@arkenea.com>
2026-05-28 20:42:13 +0530

    ui feedbacks updated

--- src/app/features/district-admin/dashboard/district-performance-summary/district-performance-summary.ts
+++ src/app/features/district-admin/dashboard/district-performance-summary/district-performance-summary.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-district-performance-summary',
@@ -13,8 +14,9 @@ import { MatSelectModule } from '@angular/material/select';
   templateUrl: './district-performance-summary.html',
   styleUrl: './district-performance-summary.scss',
 })
-export class DistrictPerformanceSummary {
-    constructor(private cdr: ChangeDetectorRef) {}
+
+export class DistrictPerformanceSummary{
+  constructor(private cdr: ChangeDetectorRef) {}
 
   @ViewChild('scrollContainer', { static: false })
   scrollContainer!: ElementRef<HTMLDivElement>;
@@ -23,48 +25,63 @@ export class DistrictPerformanceSummary {
   showNext = false;
   isScrollable = false;
 
-    stats = [
-    {
-      value: 800,
-      label: 'Points',
-      title: '2023-24',
-      color: '#D64550'
-    },
-    {
-      value: 849,
-      label: 'Points',
-      title: '2024-25',
-      color: '#DCE52A'
-    },
-    {
-      value: 900,
-      label: 'Points',
-      title: '2025-26 Goal',
-      color: '#90C955' 
-    },
-    {
-      value: 826,
-      label: 'Points',
-      title: '2025-26 Predicted',
-      color: '#6D94FF'
-    }
-  ];
-  maxValue = 1000; // for percentage calculation
+  private currentIndex = 0;
 
-  getProgress(value: number): number {
-    return (value / this.maxValue) * 100;
+  scoreData = [
+  {
+    score: 800,
+    progress: 80,
+    color: '#D64550',
+    label: '2023-24'
+  },
+  {
+    score: 849,
+    progress: 85,
+    color: '#DCE52A',
+    label: '2024-25'
+  },
+  {
+    score: 900,
+    progress: 90,
+    color: '#90C955',
+    label: '2025-26 Goal'
+  },
+  {
+    score: 826,
+    progress: 82,
+    color: '#6D94FF',
+    label: '2025-26 Predicted'
   }
-    ngAfterViewInit() {
+];
+
+  ngAfterViewInit() {
     this.updateScrollButtons();
     setTimeout(() => this.checkScroll(), 100);
+
+    AOS.init({
+      duration: 4000,
+      once: true
+    }); 
+    AOS.refresh();
   }
 
   scrollLeft() {
     if (!this.scrollContainer || !this.isScrollable) return;
 
-    this.scrollContainer.nativeElement.scrollBy({
-      left: -100,
-      behavior: 'smooth'
+    const items: HTMLElement[] = Array.from(
+      this.scrollContainer.nativeElement.querySelectorAll('.scroll-item')
+    );
+
+    if (!items.length) return;
+
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+
+    items[this.currentIndex].scrollIntoView({
+      behavior: 'smooth',
+      inline: 'start',
+      block: 'nearest'
     });
 
     setTimeout(() => this.checkScroll(), 300);
@@ -73,9 +90,20 @@ export class DistrictPerformanceSummary {
   scrollRight() {
     if (!this.scrollContainer || !this.isScrollable) return;
 
-    this.scrollContainer.nativeElement.scrollBy({
-      left: 100,
-      behavior: 'smooth'
+    const students: HTMLElement[] = Array.from(
+      this.scrollContainer.nativeElement.querySelectorAll('.scroll-item')
+    );
+
+    if (!students.length) return;
+
+    if (this.currentIndex < students.length - 1) {
+      this.currentIndex++;
+    }
+
+    students[this.currentIndex].scrollIntoView({
+      behavior: 'smooth',
+      inline: 'start',
+      block: 'nearest'
     });
 
     setTimeout(() => this.checkScroll(), 300);
@@ -93,11 +121,11 @@ export class DistrictPerformanceSummary {
       this.showPrev = false;
       this.showNext = false;
     } else {
-      this.showPrev = el.scrollLeft > 5;
-      this.showNext = el.scrollLeft < maxScrollLeft - 5;
+      this.showPrev = el.scrollLeft > 0;
+      this.showNext = el.scrollLeft < maxScrollLeft - 1;
+      this.cdr.detectChanges();
     }
-
-    this.cdr.detectChanges();
+    
   }
 
   updateScrollButtons(): void {
@@ -109,5 +137,8 @@ export class DistrictPerformanceSummary {
   @HostListener('window:resize')
   onResize(): void {
     this.updateScrollButtons();
+    setTimeout(() => {
+      this.checkScroll();
+    }, 100);
   }
 }
