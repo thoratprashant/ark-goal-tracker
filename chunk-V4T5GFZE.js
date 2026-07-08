8989bf478d50390ba527059b9b2ed49bcd923ac4
satyajitarkenea24 <satyajit@arkenea.com>
2026-07-08 13:44:00 +0530

    Auto-generated commit

---
+++ chunk-V4T5GFZE.js
@@ -0,0 +1,1 @@
+import{e as i}from"./chunk-QVGMF7SV.js";import{Qa as o,R as t}from"./chunk-34AQWMR5.js";var I=(()=>{class e{static \u0275fac=function(s){return new(s||e)};static \u0275mod=o({type:e});static \u0275inj=t({imports:[i]})}return e})();export{I as a};
