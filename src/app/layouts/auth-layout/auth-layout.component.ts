aea8ddec30f5b243b22a91395c74af96bb7898e3
satyajitarkenea24 <satyajit@arkenea.com>
2026-07-31 13:31:39 +0530

    ui

--- src/app/layouts/auth-layout/auth-layout.component.ts
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgOptimizedImage],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss',
 })
