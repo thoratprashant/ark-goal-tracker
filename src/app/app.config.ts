2e862d5e4972f283992290838efa8a8edabcb2e1
satyajitarkenea24 <satyajit@arkenea.com>
2026-08-03 11:52:17 +0530

    Ui updated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { bootstrapApplication, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { routes } from './app.routes';
+
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { App } from './app';
+
+bootstrapApplication(App, {
+  providers: [importProvidersFrom(NgApexchartsModule)]
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
     provideRouter(routes),
     provideBrowserGlobalErrorListeners(),
-    provideClientHydration(withEventReplay()),
+    provideRouter(routes), provideClientHydration(withEventReplay()),
   ]
 };
